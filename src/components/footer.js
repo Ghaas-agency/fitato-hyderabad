4eb4268670d39bebcd310423d8cadc21d84213ee
Akshay Dhiman <akshay.virgo@gmail.com>
2019-11-11 15:44:26 +0530

    refresh changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ var d = new Date();
 const Footer = () => (
   <div className="footer-copyright">
     <div className="container">
-      <p className="footer-contact-links">Call us for any queries:<br /><a href="tel:+919833736703">+91 98337 36703</a><a href="tel:+917993604033">+91 79936 04033</a></p>
+      <p className="footer-contact-links">Call us for any queries:<br /><a href="tel:+918448448546">+91 84484 48546</a></p>
       <small>&copy; {d.getFullYear()} Fitato Health Solutions Private Limited.</small>
       <div>
         <small>
